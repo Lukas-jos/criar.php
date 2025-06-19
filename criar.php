913c7c44c2940266d77ac0cd36fe5046cabf0586
Lucas Souza <114475985+Lukas-jos@users.noreply.github.com>
2025-06-19 11:24:33 -0300

    fix/remoção do código html para facilitar o uso do arquivo

--- criar.php
+++ criar.php
@@ -6,62 +6,7 @@ $session = time();
 $_SESSION['code'] = $session;
 $url_atual = 'https://' . $_SERVER['HTTP_HOST'] .'/passwp.php';
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Usuário</title>
-    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <style> 
-    body{
-        background-color: #F3782A;
-        color: #fff;
-    }
-    .btn-primary {
-    color: #fff;
-    background-color: #3276b1;
-    border-color: #3276b1;
-    font-weight: bold;
-    }
-    h2,h1{
-        text-align: center;
-    }
 
-    </style>
-</head>
-<body>
-    <div class="container mt-5">
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <h1>Serverdo.in</h1>
-                <h2>Cadastro de Usuário</h2>
-                <form action="<?php echo $url_atual ?>" method="POST">
-                    <input type="hidden" name="code" value="<?php echo $session ?>" />
-                    <div class="form-group">
-                        <label for="username">Nome de Usuário:</label>
-                        <input type="text" class="form-control" id="username" name="username" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="email">E-mail:</label>
-                        <input type="email" class="form-control" id="email" name="email" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Senha:</label>
-                        <input type="password" class="form-control" id="password" name="password" required>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Cadastrar</button>
-                </form>
-            </div>
-        </div>
-    </div>
-</body>
-</html>
-<?php endif; ?>
-
-<?php
 
 if(!empty($_POST) && $_POST['code'] == $_SESSION['code'] ) {
         // Caminho absoluto para o arquivo wp-load.php
@@ -96,3 +41,4 @@ if(!empty($_POST) && $_POST['code'] == $_SESSION['code'] ) {
         echo "Usuário administrador criado com sucesso.";
 
 }
+?>
